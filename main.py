b89708296beff35998e230114a90f0b8aab259a0
2ertwo <94830051+2ertwo@users.noreply.github.com>
2021-11-30 14:47:14 +0800

    Update main.py
    添加了注释

--- main.py
+++ main.py
@@ -11,30 +11,31 @@ def callback():
         timestamp = request.args.get("timestamp")
         nonce = request.args.get("nonce")
         echostr = request.args.get("echostr")
-        is_able = tools.urlvary(msgsignatrue, timestamp, nonce, echostr)
+        is_able = tools.urlvary(msgsignatrue, timestamp, nonce, echostr)  # 通过参数msg_signature对请求进行校验，确认调用者的合法性
         if is_able:
-            final = tools.decode_echo(echostr)
-            return final
+            final = tools.decode_echo(echostr)  # 解密echostr参数得到消息内容
+            return final  # 响应GET请求，响应内容为明文消息内容(不能加引号，不能带bom头，不能带换行符)
         else:
             return "e"
     else:
         msgsignatrue = request.args.get("msg_signature")
         timestamp = request.args.get("timestamp")
         nonce = request.args.get("nonce")
-        echostr = tools.tiqu(request.data)
-        is_able = tools.urlvary(msgsignatrue, timestamp, nonce, echostr)
+        echostr = tools.tiqu(request.data)  # 提取接收的消息包中的Encrypt参数
+        is_able = tools.urlvary(msgsignatrue, timestamp, nonce, echostr)  # 通过参数msg_signature对请求进行校验，确认调用者的合法性
         if is_able:
             msgs = tools.decode_echo(echostr)
-            text = tools.find(msgs.decode("UTF-8"))
-            app.logger.error(text)
-            err = tools.send_to("BaiJinFan", text)
-            app.logger.error(err)
+            sender, text = tools.find(msgs.decode("UTF-8"))
+            content = "%s发送了：%s" % (tools.dic[sender], text)
+            if text == 0:
+                return msgs
+            if sender == "成员1的UserId":
+                err = tools.send_to("all", content)
+            else:
+                err = tools.send_to("成员1的UserId", content)
+            app.logger.error("%s，返回码：%s" % (content, err))
             return msgs
         else:
-            tools.send_to("BaiJinFan", msgsignatrue)
-            tools.send_to("BaiJinFan", timestamp)
-            tools.send_to("BaiJinFan", nonce)
-            tools.send_to("BaiJinFan", echostr)
             return "e"
 
 
