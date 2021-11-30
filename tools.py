4a5269c49e1def7720253ecf07ee3bd010f961c0
2ertwo <94830051+2ertwo@users.noreply.github.com>
2021-11-30 14:49:08 +0800

    Update tools.py
    增加了注释

--- tools.py
+++ tools.py
@@ -1,58 +1,55 @@
 import hashlib
 from base64 import b64decode
-from Cryptodome.Cipher import AES  # 在Linux上应为Crypto
+from Crypto.Cipher import AES  # 在Windows上应为Cryptodome
 import socket
 import struct
 import xml.etree.cElementTree as ET
 import requests
 import re
-# corpId = "ww3f0728af56e6f87c"
-# token = "PVIqN3sRkFHYI"
-# encodingAesKey = "1VPNUrpKMphkcTLyjo7sgYgcfQJ8cWPF5pq4DPifWHa"
-# ouraccess = "KloHVTiqfaBBsITkjqxAzckFci_vauxm4q0O4eXPLRNqBRy_sVusJ9vHQkBXYcAYaGT0TOrdvPekweNS6Dua7k4sipCjKoaKy4icg-vz0205e0itUOreueitMYocMDs0T5HobDDjRfKg68gxBcC8-xlSrjN6Me55T-v7BL0a7LexRAkf4lUxU0L38ME4l2djlY7tvlKRimf0CudK8sbq5w"
-corpId = "ww3f0728af56e6f87c"
-corpsecret = "wd6WHoP9X_z7KqfNmVRPNC8UyHd8s_0CpxwQ4F9awJ4"
-token = "Wvmtqi"
-encodingAesKey = "cSsrSnv43DWGOCX75aagbdV5BcIk3cCBsu5IPV5J1Px"
-ouraccess = " "
-dic = {"BaiJinFan": "白锦帆",
-    "GongChenFeng": "宫晨峰",
-    "lintong": "李晓峰",
-    "8bff448a58c8315307d6d1761b0ed267": "孙阳",
-    "rearrange": "王婧怡学姐"}
+corpId = ""  # 这里是你的企业ID
+token = ""  # 这里是你自建应用的access_token
+encodingAesKey = ""  # 这里是你自建应用的编码后的AesKey
+ouraccess = ""  # 这里是你发送消息时用的access_token，第一次使用可留空
+corpsecret = ""  # 这里是你应用的Secret
+dic = {"成员1的UserId": "成员1的姓名",
+       "成员2的UserId": "成员2的姓名",
+       "成员3的UserId": "成员3的姓名",
+       "成员4的UserId": "成员4的姓名",
+       "成员5的UserId": "成员5的姓名"}
 
-def urlvary(msgsignatrue, timestamp, nonce, echostr):
-    thelist = sorted([token, timestamp, nonce, echostr])
+
+def urlvary(msgsignatrue, timestamp, nonce, echostr):  # 验证签名
+    thelist = sorted([token, timestamp, nonce, echostr])  # 字典排序
     need = ""
     for i in range(4):
-        need += thelist[i]
-    result = hashlib.sha1(need.encode("utf-8")).hexdigest()
-    if result == msgsignatrue:
+        need += thelist[i]  # 字符串拼接
+    result = hashlib.sha1(need.encode("utf-8")).hexdigest()  # 取sha1
+    if result == msgsignatrue:  # 判断签名是否相等
         return True
     else:
         return False
 
 
-def decode_echo(sth):
-    need_aes = b64decode(sth)
-    aeskey = b64decode(encodingAesKey + "=")
+def decode_echo(sth):  # 解码消息体
+    need_aes = b64decode(sth)  # 原消息base64解码
+    aeskey = b64decode(encodingAesKey + "=")  # 先对编码后的AesKey进行b64解码
     aes = AES.new(aeskey, AES.MODE_CBC, aeskey[:16])
-    plain_text = aes.decrypt(need_aes)
-    content = plain_text[16:]
-    xml_len = socket.ntohl(struct.unpack("I", content[: 4])[0])
-    xml_content = content[4:xml_len+4]
-    from_receiveid = content[xml_len+4:]
+    plain_text = aes.decrypt(need_aes)  # 用AesKey解密文本
+    content = plain_text[16:]  # 去除前面的随机数
+    xml_len = socket.ntohl(struct.unpack("I", content[: 4])[0])  # 提取消息长度
+    xml_content = content[4:xml_len+4]  # 提取xml消息
+    from_receiveid = content[xml_len+4:]  # 提取receiveid
     return xml_content
 
 
 def send_to(who, text):
-    global ouraccess
-    url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=" + ouraccess
-    if who == "all":
-        target = "BaiJinFan|GongChenFeng|lintong|8bff448a58c8315307d6d1761b0ed267"
+    global ouraccess  # 声明全局变量
+    url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=" + ouraccess  # 请求地址
+    if who == "all":  # 判断发送目标
+        target = "成员1id|成员2id|成员3id|成员4id|成员5id"  # 群发所有人
     else:
-        target = who
-    data ={
+        target = who  # 单发某个人
+    data = {
         "touser": target,
         "msgtype": "text",
         "agentid": 1000003,
@@ -62,39 +59,35 @@ def send_to(who, text):
         "safe": 0,
         "enable_id_trans": 0,
         "enable_duplicate_check": 0
-        }
-    req = requests.post(url, json=data)
-    errcode = re.findall(r"errcode\":(.+?),\"errmsg", req.text)[0]
-    if errcode == "40014" or errcode == "42001":
-        ouraccess = get_access_token()
-        send_to(who, text)
+        }  # 消息体
+    req = requests.post(url, json=data)  # 发送Post请求
+    errcode = re.findall(r"errcode\":(.+?),\"errmsg", req.text)[0]  # 提取返回码
+    if errcode == "40014" or errcode == "42001":  # 40014和42001都代表access_token出问题
+        ouraccess = get_access_token()  # 重新获取access_token
+        send_to(who, text)  # 重新发送
     else:
-        return errcode
+        return errcode  # 返回返回码以供可能的Debug
 
 
-def tiqu(str):
+def tiqu(str):  # 提取正文消息
     xml_tiqu = ET.fromstring(str)
     encrypt = xml_tiqu.find("Encrypt")
     return encrypt.text
 
 
-def get_access_token():
-    url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=' + corpId + '&corpsecret=' + corpsecret
-    req = requests.get(url)
-    new = re.findall(r"access_token\":\"(.+?)\",\"expires_in", req.text)
+def get_access_token():  # 获得access_token
+    url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=' + corpId + '&corpsecret=' + corpsecret  # 构建url
+    req = requests.get(url)  # 发送Get请求
+    new = re.findall(r"access_token\":\"(.+?)\",\"expires_in", req.text)  # 提取返回的access_token
     return new[0]
 
 
 def find(sth):
     text = [0]
-    msg_type = re.findall(r"<MsgType><!\[CDATA\[(.+?)\]\]></MsgType>", sth)
-    sender = re.findall(r"<FromUserName><!\[CDATA\[(.+?)\]\]></FromUserName>", sth)
+    msg_type = re.findall(r"<MsgType><!\[CDATA\[(.+?)\]\]></MsgType>", sth)  # 提取信息类型参数
+    sender = re.findall(r"<FromUserName><!\[CDATA\[(.+?)\]\]></FromUserName>", sth)  # 提取发送者参数
     if msg_type[0] != "text":
-        text[0] = sth
+        text[0] = 0
     else:
-        text = re.findall(r"<Content><!\[CDATA\[(.+?)\]\]></Content>", sth)
-    return "%s发送了%s" % (sender[0], text[0])
-
-
-if __name__ == '__main__':
-    print(urlvary("477715d11cdb4164915debcba66cb864d751f3e6", "1409659813", "1372623149", "RypEvHKD8QQKFhvQ6QleEB4J58tiPdvo+rtK1I9qca6aM/wvqnLSV5zEPeusUiX5L5X/0lWfrf0QADHHhGd3QczcdCUpj911L3vg3W/sYYvuJTs3TUUkSUXxaccAS0qhxchrRYt66wiSpGLYL42aM6A8dTT+6k4aSknmPj48kzJs8qLjvd4Xgpue06DOdnLxAUHzM6+kDZ+HMZfJYuR+LtwGc2hgf5gsijff0ekUNXZiqATP7PF5mZxZ3Izoun1s4zG4LUMnvw2r+KqCKIw+3IQH03v+BCA9nMELNqbSf6tiWSrXJB3LAVGUcallcrw8V2t9EL4EhzJWrQUax5wLVMNS0+rUPA3k22Ncx4XXZS9o0MBH27Bo6BpNelZpS+/uh9KsNlY6bHCmJU9p8g7m3fVKn28H3KDYA5Pl/T8Z1ptDAVe0lXdQ2YoyyH2uyPIGHBZZIs2pDBS8R07+qN+E7Q=="))
+        text = re.findall(r"<Content><!\[CDATA\[(.+?)\]\]></Content>", sth)  # 提取正文
+    return sender[0], text[0]
